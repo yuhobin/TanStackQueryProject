e827fb38e48bdfb5630d0f53365e9d4e9d70bfcc
yuhobin <dptmzh12@gmail.com>
2026-09-18 10:16:12 +0900

    yuhobin)repository 생성

---
+++ src/main/java/com/sist/web/TanStackQueryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanStackQueryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanStackQueryProjectApplication.class, args);
+	}
+
+}
