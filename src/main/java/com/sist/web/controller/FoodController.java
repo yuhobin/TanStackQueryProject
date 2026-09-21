af1b032dba15ffe7cd1f6300e8b65ad3cc5b3cfa
yuhobin <dptmzh12@gmail.com>
2026-09-21 10:36:06 +0900

    yuhobin) fix: docker-compose

--- src/main/java/com/sist/web/controller/FoodController.java
+++ src/main/java/com/sist/web/controller/FoodController.java
@@ -32,7 +32,7 @@ import java.util.*;
  * 
  * 	==> 동작 확인 여부
  * 
- * 테스트5 docker 불필요 용량 삭제 디스크 메모리 확보 후 재시도(Jenkins restart 진행)
+ * 테스트5 docker 불필요 용량 삭제 디스크 메모리 확보 후 재시도(Jenkins restart 진행) 수정
  * 
  */
 @Controller
